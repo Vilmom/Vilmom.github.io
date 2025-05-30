ac7fb4218ae64e25acfb8c461e4178aa24e74f9b
Vilson <97314427+Vilmom@users.noreply.github.com>
2025-05-30 00:34:11 -0400

    Moved index.jsx to front

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { LandingPage } from "./screens/landingPage/landingPage";
+import { LandingPage } from "./frontend/src/screens/landingPage/landingPage";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
